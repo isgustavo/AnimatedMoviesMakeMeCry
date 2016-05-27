c38331451569e7f3220949b17b1641189bca0428
isgustavo <hi.gustavo.oliveira@gmail.com>
2016-05-27 11:44:32 -0300

    Add Firebase pod to iOS project

---
+++ iOS/Animated Movies Make Me Cry/Pods/Target Support Files/Pods-Animated Movies Make Me Cry/Pods-Animated Movies Make Me Cry-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Animated_Movies_Make_Me_CryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Animated_Movies_Make_Me_CryVersionString[];
+
---
+++ iOS/Animated Movies Make Me Cry/Pods/Target Support Files/Pods-Animated Movies Make Me CryTests/Pods-Animated Movies Make Me CryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Animated_Movies_Make_Me_CryTests : NSObject
+@end
+@implementation PodsDummy_Pods_Animated_Movies_Make_Me_CryTests
+@end
